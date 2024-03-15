e8b64682116724ac65a493662b49205ea953176d
Abubakar1010 <abubakar850772@gmail.com>
2024-03-15 19:09:38 +0600

    project initialized with react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <p className='bg-pink-800 text-xl from-black p-8'>Project setup with react and tailwind css </p>
+    
+    </>
+  )
+}
+
+export default App
